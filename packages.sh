f14907064216aa5a5e30cb7494a22a13dec7728e
Yu-Jhe Li <yujhe.li@appier.com>
2020-03-14 12:50:47 +0800

    Define installed packages into variable

---
+++ packages.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PACKAGES=(
+  "anyenv"
+  "bat"
+  "diff-so-fancy"
+  "docker"
+  "fzf"
+  "git"
+  "jq"
+  "tldr"
+  "tmux"
+  "tmux-plugins"
+  "tree"
+)
+
+BASH_PACKAGES=(
+  "bash-it"
+  "bash-completion"
+)
+
+ZSH_PACKAGES=(
+  "oh-my-zsh"
+  "zsh-completions"
+  "zsh-syntax-highlighting"
+)
